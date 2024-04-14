8fbbf1bfb2178c298dad9c3cc344126c222dee25
lukas <lkab05@hotmail.com>
2024-04-14 12:45:31 -0300

    feat: added hashcode and equals

--- src/main/java/com/lucasbalves/commerce/entities/helper/OrderItemPK.java
+++ src/main/java/com/lucasbalves/commerce/entities/helper/OrderItemPK.java
@@ -7,6 +7,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
 public class OrderItemPK implements Serializable { // Chave composta
@@ -41,5 +42,18 @@ public class OrderItemPK implements Serializable { // Chave composta
 
     public void setProduct(Product product) {
         this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemPK that = (OrderItemPK) o;
+        return Objects.equals(order, that.order) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(order, product);
     }
 }
