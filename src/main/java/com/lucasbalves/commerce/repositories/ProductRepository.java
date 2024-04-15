67088263021c540f464642b381b8ae6852de9066
lukas <lkab05@hotmail.com>
2024-04-15 16:34:24 -0300

    feat: add repository layer for products

---
+++ src/main/java/com/lucasbalves/commerce/repositories/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.lucasbalves.commerce.repositories;
+
+import com.lucasbalves.commerce.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
